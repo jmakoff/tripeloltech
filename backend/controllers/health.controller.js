a80715b1cf90c83884938f83ea553af7f96362d1
Yurii Makov <y.makov@p1k.org>
2026-04-09 12:19:33 +0200

    fixed /readyz endpoint

--- backend/controllers/health.controller.js
+++ backend/controllers/health.controller.js
@@ -1,4 +1,5 @@
 const { version } = require('../../package.json');
+const mockDataService = require('../services/mockData.service');
 const NAME = 'eco-platform-api';
 
 exports.healthz = async (_req, res) => {
@@ -6,7 +7,13 @@ exports.healthz = async (_req, res) => {
 };
 
 exports.readyz = async (_req, res) => {
-  return res.status(200).send({ status: 'ready' });
+  const checks = { dataService: mockDataService.getStatus() };
+
+  if (!mockDataService.isReady()) {
+    return res.status(503).send({ status: 'not_ready', checks });
+  }
+
+  return res.status(200).send({ status: 'ready', checks });
 };
 
 exports.getVersion = async (_req, res) => {
