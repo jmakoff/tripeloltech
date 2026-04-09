efde5af1033b9d8bdce529007f7afdacda1f6251
Yurii Makov <y.makov@p1k.org>
2026-04-09 11:55:00 +0200

    added version endpoint

--- backend/controllers/health.controller.js
+++ backend/controllers/health.controller.js
@@ -1,3 +1,6 @@
+const { version } = require('../../package.json');
+const NAME = 'eco-platform-api';
+
 exports.healthz = async (_req, res) => {
   return res.status(200).send({ status: 'ok' });
 };
@@ -6,4 +9,14 @@ exports.readyz = async (_req, res) => {
   return res.status(200).send({ status: 'ready' });
 };
 
-
+exports.getVersion = async (_req, res) => {
+  try {
+    return res.status(200).send({
+      name: NAME,
+      version,
+      env: process.env.NODE_ENV,
+    });
+  } catch (_e) {
+    return res.status(200).send({ success: false, msg: 'Internal error' });
+  }
+};
